42da2f05e82cff5f1b34ddabf08a672c15a94f1d
a-sardorbek <sabduvahobov01@gmail.com>
2022-06-03 14:00:12 +0500

    Multi-module project using gradle

--- core/src/main/java/com/system/core/Utils.java
+++ core/src/main/java/com/system/core/Utils.java
@@ -4,13 +4,11 @@ import com.system.v1.StringUtils;
 public class Utils {
 
     public static boolean isAllPositiveNumbers(String... str) {
-
         for (int i = 0; i < str.length; i++) {
             if (!StringUtils.isPositiveNumber(str[i])) {
                 return false;
             }
         }
         return true;
-
     }
 }
